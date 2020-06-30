85fcb39eee08a830570283f7d3ff5d55a0bf4a54
Ahmed AlSalih <Ahmed.Al-Salih@retailbusinessservices.com>
2020-06-29 23:08:46 -0400

    springboot Batch job project reading from MSSQL database

---
+++ src/main/java/com/atr/config/BatchConfiguration.java
@@ -0,0 +1,10 @@
+package com.atr.config;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfiguration {
+
+}
