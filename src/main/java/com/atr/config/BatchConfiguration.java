8c067eb42c9d9d3008030516f86f3295314498e2
Ahmed AlSalih <Ahmed.Al-Salih@retailbusinessservices.com>
2020-06-29 23:42:01 -0400

    add database configuration and iqnore the database connuiction details

--- src/main/java/com/atr/config/BatchConfiguration.java
+++ src/main/java/com/atr/config/BatchConfiguration.java
@@ -1,10 +1,49 @@
 package com.atr.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 @EnableBatchProcessing
 public class BatchConfiguration {
+	
+	@Autowired
+	public JobBuilderFactory jobBuilderFactory;
+	
+	@Autowired
+	public StepBuilderFactory stepBuilderFactory;
+	
+	@Autowired
+	public DataSource dataSource;
+	
+	
+	@Value("${db1.mssql.user}")
+    private String msssqlUser;
+	
+	@Value("${db1.mssql.password}")
+    private String msssqlpassword;
+
+    @Value("${db1.mssql.url}")
+    private String mssqlUrl;
+	
+	@Bean
+	public DataSource dataSource() {
+		
+		final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		dataSource.setUrl(mssqlUrl);
+		dataSource.setUsername(msssqlUser);
+		dataSource.setPassword(msssqlpassword);
+		return dataSource;
+		
+	}
 
 }
